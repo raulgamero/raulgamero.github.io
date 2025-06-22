b4095d0474065593d615982d41645b272fc9085f
gamerinha <raulgamero03@gmail.com>
2025-06-22 21:49:45 +0000

    created experience section

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
+  document.addEventListener('DOMContentLoaded', () => {
+    const items = document.querySelectorAll('.timeline-item');
+
+    function mostrarItems() {
+      const triggerBottom = window.innerHeight * 0.85;
+
+      items.forEach(item => {
+        const itemTop = item.getBoundingClientRect().top;
+        if (itemTop < triggerBottom) {
+          item.classList.add('visible');
+        } else {
+          item.classList.remove('visible');
+        }
+      });
+    }
+
+    window.addEventListener('scroll', mostrarItems);
+    mostrarItems();
+  });
+
 const toggleMenuElement = document.getElementById('mobile-menu')
 const mainMenuElement = document.getElementById('closed-menu')
 const toggle2 = document.getElementById('cerrar')
